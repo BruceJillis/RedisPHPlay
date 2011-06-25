7f726545f37441f6dd8d9ef8a0ed755c36081261
Jillis ter Hove <j.terhove@gmail.com>
2011-06-25 23:00:29 +0200

    updated and expanded the hashes example

--- examples/hashes.php
+++ examples/hashes.php
@@ -14,5 +14,6 @@
 	printf("COMMAND arguments (= expected) -> actual\n");
 	printf("\n");
 
-	printf("HMSET array('a' => 1, 'b' => 2) (= 0) -> %d\n", $client->HMSET('foo', array('a' => 1, 'b' => 2)));
+	printf("HMSET a = 1, b = 2 (= 0) -> %d\n", $client->HMSET('foo', 'a', 1, 'b', 2));
+	printf("HGETALL foo (= array(a=1, b=2)) -> %s\n", print_r($client->HGETALL('foo'), 1));
 	printf("\n");
