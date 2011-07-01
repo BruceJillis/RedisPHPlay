239855311575b37752faca363fef3cfc071d615d
Jillis ter Hove <j.terhove@gmail.com>
2011-07-01 22:59:38 +0200

    updated pipelining example to make use of the new Benchmark features

--- examples/pipelining.php
+++ examples/pipelining.php
@@ -16,17 +16,19 @@
 	$redis = new RedisManager();
 	$client = $redis->connect('127.0.0.1', 6379);
 	$client->auth('test');
+	
+	for($j = 0; $j < 5; $j++ ) {
+		$benchmark->start('normal');
+		for($i = 0; $i <= ($times * $batch); $i++)  
+			$client->SET('a_' . $i, $i);
+		$benchmark->stop('normal');
 
-	$benchmark->start('normal');
-	for($i = 0; $i <= ($times * $batch); $i++)  
-		$client->SET('a_' . $i, $i);
-	$benchmark->stop('normal');
-
-	$benchmark->start('pipeline');
-	$pipeline = $client->pipeline($batch); // start pipelining on $batch nr of commands
-	for($i = 0; $i <= ($times * $batch); $i++)  
-		$pipeline->SET('a_' . $i, $i);
-	$pipeline->flush(); // make sure any leftover commands are sent
-	$benchmark->stop('pipeline');
+		$benchmark->start('pipeline');
+		$pipeline = $client->pipeline($batch); // start pipelining on $batch nr of commands
+		for($i = 0; $i <= ($times * $batch); $i++)  
+			$pipeline->SET('a_' . $i, $i);
+		$pipeline->flush(); // make sure any leftover commands are sent
+		$benchmark->stop('pipeline');
+	}
 
 	$benchmark->display();
